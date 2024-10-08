c7e974160d729a74966f9ec7bfae19f42a27e562
Ahnaf Shahriar <bdahnaf@gmail.com>
2024-10-08 21:41:15 +0600

    Added DbContext and then added Categories Table reference

---
+++ ExpenseTrackerWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ExpenseTrackerWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseTrackerWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
