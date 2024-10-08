783551f81cac948a6bbbd06dcd13e8b2c86b8f89
Ahnaf Shahriar <bdahnaf@gmail.com>
2024-10-08 21:29:00 +0600

    Add Data Annotations

--- ExpenseTrackerWeb/Models/Category.cs
+++ ExpenseTrackerWeb/Models/Category.cs
@@ -1,8 +1,12 @@
-﻿namespace ExpenseTrackerWeb.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExpenseTrackerWeb.Models
 {
     public class Category
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
     }
