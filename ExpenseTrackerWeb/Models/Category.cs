e03d010ac6ad286f6039bcd03cf8f68eef1610c3
Ahnaf Shahriar <bdahnaf@gmail.com>
2024-10-08 21:28:41 +0600

    Add Category model

---
+++ ExpenseTrackerWeb/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace ExpenseTrackerWeb.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+    }
+}
