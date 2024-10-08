db8bafa2272dd52ea6655ec0a1fd462728360eb9
Ahnaf Shahriar <bdahnaf@gmail.com>
2024-10-08 23:10:56 +0600

    DI

--- ExpenseTrackerWeb/Controllers/CategoryController.cs
+++ ExpenseTrackerWeb/Controllers/CategoryController.cs
@@ -1,11 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ExpenseTrackerWeb.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ExpenseTrackerWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
+            var objCategoryList = _db.Categories.ToList();
             return View();
         }
     }
