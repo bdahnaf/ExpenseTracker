dfaf4065b69af202aac64e633900b2bc88e7b05d
Ahnaf Shahriar <bdahnaf@gmail.com>
2024-10-09 21:47:10 +0600

    Add seed data to Category table

--- ExpenseTrackerWeb/Data/ApplicationDbContext.cs
+++ ExpenseTrackerWeb/Data/ApplicationDbContext.cs
@@ -10,5 +10,14 @@ namespace ExpenseTrackerWeb.Data
             
         }
         public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id= 1, Name = "Utilities" },
+                new Category { Id = 2, Name = "Transport" },
+                new Category { Id = 3, Name = "Foods" }
+                );
+        }
     }
 }
---
+++ ExpenseTrackerWeb/Migrations/20241009154537_SeedCategory.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace ExpenseTrackerWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedCategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "CreatedDateTime", "Name" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2024, 10, 9, 21, 45, 36, 283, DateTimeKind.Local).AddTicks(156), "Utilities" },
+                    { 2, new DateTime(2024, 10, 9, 21, 45, 36, 283, DateTimeKind.Local).AddTicks(166), "Transport" },
+                    { 3, new DateTime(2024, 10, 9, 21, 45, 36, 283, DateTimeKind.Local).AddTicks(167), "Foods" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
