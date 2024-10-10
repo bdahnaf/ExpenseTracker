8a4b38ef090f2a35ecc920a99841a669aa242a82
Ahnaf Shahriar <bdahnaf@gmail.com>
2024-10-10 15:45:59 +0600

    Add Unique Constraint to Category Name in Category Table

---
+++ ExpenseTrackerWeb/Migrations/20241010094325_AddUniqueConstraintToCategoryNameInCategoryTable.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExpenseTrackerWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUniqueConstraintToCategoryNameInCategoryTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Categories",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDateTime",
+                value: new DateTime(2024, 10, 10, 15, 43, 24, 186, DateTimeKind.Local).AddTicks(4128));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDateTime",
+                value: new DateTime(2024, 10, 10, 15, 43, 24, 186, DateTimeKind.Local).AddTicks(4153));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedDateTime",
+                value: new DateTime(2024, 10, 10, 15, 43, 24, 186, DateTimeKind.Local).AddTicks(4154));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Categories_Name",
+                table: "Categories",
+                column: "Name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Categories_Name",
+                table: "Categories");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Categories",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDateTime",
+                value: new DateTime(2024, 10, 9, 21, 45, 36, 283, DateTimeKind.Local).AddTicks(156));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDateTime",
+                value: new DateTime(2024, 10, 9, 21, 45, 36, 283, DateTimeKind.Local).AddTicks(166));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedDateTime",
+                value: new DateTime(2024, 10, 9, 21, 45, 36, 283, DateTimeKind.Local).AddTicks(167));
+        }
+    }
+}
