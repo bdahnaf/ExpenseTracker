d5c0044508432b2d132d9ee237f7ad1ef4c16729
Ahnaf Shahriar <bdahnaf@gmail.com>
2024-10-10 15:47:31 +0600

    Add Unique Constraint to Category Name in Category Table

--- ExpenseTrackerWeb/Data/ApplicationDbContext.cs
+++ ExpenseTrackerWeb/Data/ApplicationDbContext.cs
@@ -7,17 +7,20 @@ namespace ExpenseTrackerWeb.Data
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
-            
+
         }
         public DbSet<Category> Categories { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Category>().HasData(
-                new Category { Id= 1, Name = "Utilities" },
+                new Category { Id = 1, Name = "Utilities" },
                 new Category { Id = 2, Name = "Transport" },
                 new Category { Id = 3, Name = "Foods" }
                 );
+            modelBuilder.Entity<Category>()
+                .HasIndex(c => c.Name)
+                .IsUnique();
         }
     }
 }
