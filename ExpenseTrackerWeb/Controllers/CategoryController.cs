166b1ffba63e6f9b5fa3a84993a1a8d48a43021d
Ahnaf Shahriar <bdahnaf@gmail.com>
2024-10-13 20:47:20 +0600

    Delete functionality complete

--- ExpenseTrackerWeb/Controllers/CategoryController.cs
+++ ExpenseTrackerWeb/Controllers/CategoryController.cs
@@ -52,7 +52,7 @@ namespace ExpenseTrackerWeb.Controllers
                 return NotFound();
             }
             Category category = _db.Categories.FirstOrDefault(c => c.Id == id);
-            if(category == null)
+            if (category == null)
             {
                 return NotFound();
             }
@@ -81,6 +81,31 @@ namespace ExpenseTrackerWeb.Controllers
 
             }
             return View();
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category category = _db.Categories.FirstOrDefault(c => c.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Category? category = _db.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(category);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
