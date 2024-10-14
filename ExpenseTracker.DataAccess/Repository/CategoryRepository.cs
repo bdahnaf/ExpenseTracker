1e81ad07a6ee881b94baafbc5f4dc18eb2220dfd
Ahnaf-DDC-LPTP <bdahnaf@gmail.com>
2024-10-14 16:27:22 +0600

    Add Category Repository

---
+++ ExpenseTracker.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExpenseTracker.DataAccess.Repository.IRepository;
+using ExpenseTrackerWeb.Data;
+using ExpenseTrackerWeb.Models;
+
+namespace ExpenseTracker.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Category obj)
+        {
+            _db.Categories.Update(obj);
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
---
+++ ExpenseTracker.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExpenseTrackerWeb.Models;
+
+namespace ExpenseTracker.DataAccess.Repository.IRepository
+{
+    internal interface ICategoryRepository : IRepository<Category>
+    {
+        void Update(Category obj);
+        void Save();
+    }
+}
